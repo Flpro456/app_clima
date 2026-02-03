9af1d0c8fb22092618a126687e53848a57bd30a7
Florencio Lazo <flazozegers@gmail.com>
2026-02-03 11:26:00 -0300

    Completado el archivo geocodificador.py

---
+++ geocodificador.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import os
+
+ciudad = input("Ingresa la ciudad: ")
+
+answer = requests.get(f"https://geocoding-api.open-meteo.com/v1/search?name={ciudad}&count=1&language=es&format=json")
+answer = dict(json.loads(answer.text))
+
+
+with open("answer.json", "w") as file:
+    list = [
+        (answer)["results"][0]["latitude"],
+        (answer)["results"][0]["longitude"],
+        (answer)["results"][0]["country"]
+    ]
+    json.dump(list, file)
