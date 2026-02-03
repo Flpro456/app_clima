a675e71fc12fa2e72edab81ab27a504907c611ae
Florencio Lazo <flazozegers@gmail.com>
2026-02-03 11:55:32 -0300

    agregado gitignore

--- geocodificador.py
+++ geocodificador.py
@@ -7,9 +7,10 @@ ciudad = input("Ingresa la ciudad: ")
 answer = requests.get(f"https://geocoding-api.open-meteo.com/v1/search?name={ciudad}&count=1&language=es&format=json")
 answer = dict(json.loads(answer.text))
 
-
-list = [
-    (answer)["results"][0]["latitude"],
-    (answer)["results"][0]["longitude"],
-    (answer)["results"][0]["country"]
-]
+with open("list.json", "w") as file:
+    lista = [
+        (answer)["results"][0]["latitude"],
+        (answer)["results"][0]["longitude"],
+        (answer)["results"][0]["country"]
+    ]
+    json.dump(lista, file, indent=2)
