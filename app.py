74eefbe8243a9368b942d9ae5f5b1fe9909b2a5d
Florencio Lazo <flazozegers@gmail.com>
2026-02-04 11:34:56 -0300

    Cambio parametros clase ciudad

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from clima import data, lat, lon
 from geocodificador import ciudad, lista
 from datetime import datetime
+import json
 
 class Ciudad():
-    nombre = ciudad
-    pais = lista["pais"]
-    latitud = lat
-    longitud = lon
-    historial = []
+    def __init__(self, nombre, pais, latitud, longitud, historial):
+        Ciudad.nombre = nombre
+        Ciudad.pais = pais
+        Ciudad.latitud = latitud
+        Ciudad.longitud = longitud
+        Ciudad.historial = historial
 
     def registrar_clima(temp):
         temp = data["temp"]
@@ -22,6 +24,21 @@ class Ciudad():
         else:
             return "Clima estable"
 
-c1 = Ciudad()
+c1 = Ciudad(ciudad, lista["pais"], lat, lon, [])
 
 c1.analizar_riesgo()
+c1.registrar_clima()
+
+city = {
+    c1.nombre: {
+        "pais": c1.pais,
+        "coordenadas": [c1.latitud, c1.longitud],
+        "historial": c1.historial
+    }
+}
+
+try:
+    with open("monitoreo_floro.json", "w") as file:
+        json.dump(city, file, indent=4)
+except Exception as e:
+    print(e)
--- geocodificador.py
+++ geocodificador.py
@@ -6,9 +6,11 @@ ciudad = input("Ingresa la ciudad: ")
 answer = requests.get(f"https://geocoding-api.open-meteo.com/v1/search?name={ciudad}&count=1&language=es&format=json")
 answer = dict(json.loads(answer.text))
 
-
-lista = {
-    "latitud": (answer)["results"][0]["latitude"],
-    "longitud": (answer)["results"][0]["longitude"],
-    "pais": (answer)["results"][0]["country"]
-}
+try:
+    lista = {
+        "latitud": (answer)["results"][0]["latitude"],
+        "longitud": (answer)["results"][0]["longitude"],
+        "pais": (answer)["results"][0]["country"]
+    }
+except KeyError:
+    print("O escribiste mal el nombre, o escribiste cualquier cosa.\nDe cualquier manera, ERROR")
